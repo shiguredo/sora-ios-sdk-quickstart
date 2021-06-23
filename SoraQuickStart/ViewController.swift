315eb6cab2c2fe5f9ce85bdd8774ee876a6b5297
SUZUKI Tetsuya <szktty@shiguredo.jp>
2021-06-23 16:15:33 +0900

    接続処理を実装し直す

--- SoraQuickStart/ViewController.swift
+++ SoraQuickStart/ViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import Sora
 
 // 接続するサーバーのシグナリング URL
-let soraURL = URL(string: "ws://192.168.0.2:5000/signaling")!
+let soraURL = URL(string: "ws://192.168.0.4:5000/signaling")!
 
 // チャネル ID
 let soraChannelId = "sora"
@@ -12,10 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var senderVideoView: VideoView!
     @IBOutlet weak var receiverVideoView: VideoView!
     @IBOutlet weak var connectImageView: UIImageView!
-    
-    var senderMediaChannel: MediaChannel?
-    var receiverMediaChannel: MediaChannel?
-    var connecting = false
+
+    // 接続済みの MediaChannel です。
+    var mediaChannel: MediaChannel?
+
+    // 接続試行中にキャンセルするためのオブジェクトです。
+    var connectionTask: ConnectionTask?
+
+    // 接続済みであれば true を返します。
+    var connecting: Bool {
+        mediaChannel?.isAvailable == true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,80 +31,85 @@ class ViewController: UIViewController {
         navigationItem.title = "\(soraChannelId)"
     }
 
-    @IBAction func connect(_ sender: AnyObject) {
-        if connecting {
-            connectImageView.image = UIImage(systemName: "play.circle.fill")
-            connectImageView.tintColor = .systemGreen
-
-        } else {
-            connectImageView.image = UIImage(systemName: "stop.circle.fill")
-            connectImageView.tintColor = .systemRed
+    // 接続ボタンの UI を更新します。
+    func updateUI(_ connect: Bool) {
+        DispatchQueue.main.async {
+            if connect {
+                // 接続時の処理です。
+                self.connectImageView.image = UIImage(systemName: "stop.circle.fill")
+                self.connectImageView.tintColor = .systemRed
+            } else {
+                // 接続解除時の処理です。
+                self.connectImageView.image = UIImage(systemName: "play.circle.fill")
+                self.connectImageView.tintColor = .systemGreen
+            }
         }
-        connecting = !connecting
     }
 
-    func connect(role: Role,
-                 connectButton: UIButton,
-                 videoView: VideoView,
-                 completionHandler: @escaping (MediaChannel?) -> Void) {
-        DispatchQueue.main.async {
-            connectButton.isEnabled = false
+    @IBAction func connect(_ sender: AnyObject) {
+        // 接続試行中のタスクが残っていればキャンセルします。
+        connectionTask?.cancel()
+
+        if connecting {
+            // 接続済みであれば接続を解除します。
+            if mediaChannel?.isAvailable == true {
+                mediaChannel?.disconnect(error: nil)
+            }
+            updateUI(false)
+        } else {
+            // 未接続なら接続します。
+            connect()
+            updateUI(true)
         }
-        
+    }
+
+    func connect() {
         // 接続の設定を行います。
         let config = Configuration(url: soraURL,
                                    channelId: soraChannelId,
-                                   role: role,
+                                   role: .sendrecv,
                                    multistreamEnabled: true)
 
-        if role == .recvonly {
-            config.peerChannelHandlers.onAddStream = { mediaStream -> Void in
-                mediaStream.videoRenderer = videoView
-            }
+        // 受信した映像を描画する設定を行います。
+        // マルチストリームで接続しますが、最初に同チャネルに接続された 1 つの受信ストリームにのみ対応しています。
+        config.peerChannelHandlers.onAddStream = { mediaStream in
+            mediaStream.videoRenderer = self.receiverVideoView
         }
-        
+
         // 接続します。
-        // connect() の戻り値 ConnectionTask はここでは使いませんが、
+        // connect() の戻り値 ConnectionTask を使うと
         // 接続試行中の状態を強制的に終了させることができます。
-        let _ = Sora.shared.connect(configuration: config) { mediaChannel, error in
+        connectionTask = Sora.shared.connect(configuration: config) { mediaChannel, error in
             // 接続に失敗するとエラーが渡されます。
             if let error = error {
-                print(error.localizedDescription)
-                DispatchQueue.main.async {
-                    connectButton.isEnabled = true
-                }
-                completionHandler(nil)
+                NSLog(error.localizedDescription)
+                self.updateUI(false)
                 return
             }
-            
-            // 接続できたら VideoView をストリームにセットします。
-            // マルチストリームの場合、最初に接続したストリームが mainStream です。
-            // 受信専用で接続したとき、何も配信されていなければ mainStream は nil です。
-            if let stream = mediaChannel!.mainStream {
-                stream.videoRenderer = videoView
+
+            // 接続に成功した MediaChannel を保持しておきます。
+            self.mediaChannel = mediaChannel
+
+            // 接続できたら配信用の VideoView をストリームにセットします。
+            if let stream = mediaChannel!.senderStream {
+                stream.videoRenderer = self.senderVideoView
             }
-            
-            DispatchQueue.main.async {
-                connectButton.isEnabled = true
-                connectButton.setImage(UIImage(systemName: "stop.fill"),
-                                       for: .normal)
+
+            // すでに他のユーザーからの接続があれば受信用の VideoView をストリームにセットします。
+            // このアプリでは 1 つのストリームのみを描画します。
+            if let stream = mediaChannel!.receiverStreams.first {
+                stream.videoRenderer = self.receiverVideoView
             }
-            
-            completionHandler(mediaChannel!)
+
+            // 接続先から接続を解除されたときに行う処理です。
+            mediaChannel!.handlers.onDisconnect = { error in
+                if let error = error {
+                    NSLog(error.localizedDescription)
+                }
+                self.updateUI(false)
+            }
         }
     }
-    
-    func disconnect(mediaChannel: MediaChannel, connectButton: UIButton) {
-        if mediaChannel.isAvailable {
-            // 接続解除します。
-            mediaChannel.disconnect(error: nil)
-        }
-        
-        DispatchQueue.main.async {
-            connectButton.setImage(UIImage(systemName: "play.fill"),
-                                   for: .normal)
-        }
-    }
-    
+
 }
 
