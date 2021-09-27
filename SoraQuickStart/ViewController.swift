90d81e045156c15086373ebd8268efb5f722e8ab
miosakuma <mio.sakuma.gml@gmail.com>
2021-09-27 14:27:01 +0900

    テストコードを追加する

--- SoraQuickStart/ViewController.swift
+++ SoraQuickStart/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var senderVideoView: VideoView!
     @IBOutlet weak var receiverVideoView: VideoView!
     @IBOutlet weak var connectImageView: UIImageView!
+    //
+    var camera: CameraVideoCapturer?
+    var isFront = true
 
     // 接続済みの MediaChannel です。
     var mediaChannel: MediaChannel?
@@ -54,8 +57,21 @@ class ViewController: UIViewController {
             // 接続済みであれば接続を解除します。
             if mediaChannel?.isAvailable == true {
                 mediaChannel?.disconnect(error: nil)
+                if CameraVideoCapturer.front.isRunning {
+                    CameraVideoCapturer.front.stop() { error in
+                        if let error = error {
+                            NSLog(error.localizedDescription)
+                        }
+                    }
+                }
+                if CameraVideoCapturer.back.isRunning {
+                    CameraVideoCapturer.back.stop { error in
+                        if let error = error {
+                            NSLog(error.localizedDescription)
+                        }
+                    }
+                }
             }
-            mediaChannel = nil
             updateUI(false)
         } else {
             // 未接続なら接続します。
@@ -66,30 +82,103 @@ class ViewController: UIViewController {
 
     func connect() {
         // 接続の設定を行います。
-        let config = Configuration(url: soraURL,
+        var config = Configuration(url: soraURL,
                                    channelId: soraChannelId,
                                    role: .sendrecv,
                                    multistreamEnabled: true)
         
         // ストリームが追加されたら受信用の VideoView をストリームにセットします。
         // このアプリでは、複数のユーザーが接続した場合は最後のユーザーの映像のみ描画します。
-        config.mediaChannelHandlers.onAddStream = { [weak self] stream in
-            guard let strongSelf = self else {
-                return
-            }
+        config.mediaChannelHandlers.onAddStream = {stream in
             if stream.streamId != config.publisherStreamId {
-                stream.videoRenderer = strongSelf.receiverVideoView
+                stream.videoRenderer = self.receiverVideoView
             }
         }
         // 接続先から接続を解除されたときに行う処理です。
-        config.mediaChannelHandlers.onDisconnect = { [weak self] error in
-            guard let strongSelf = self else {
-                return
-            }
+        config.mediaChannelHandlers.onDisconnect = { error in
             if let error = error {
                 NSLog(error.localizedDescription)
             }
-            strongSelf.updateUI(false)
+            self.updateUI(false)
+        }
+
+        // カメラを起動しない
+        config.cameraSettings = CameraSettings(isEnabled: false)
+
+//        let frontCameraHandlers = CameraVideoCapturerHandlers()
+//        frontCameraHandlers.onCapture = {
+//            frame in
+//            //NSLog("front camera onCapture")
+//            return frame
+//        }
+//        frontCameraHandlers.onStart = {
+//            NSLog("front camera onStart")
+//        }
+//        frontCameraHandlers.onStop = {
+//            NSLog("front camera onStop")
+//        }
+//
+//        let backCameraHandlers = CameraVideoCapturerHandlers()
+//        backCameraHandlers.onCapture = {
+//            frame in
+//            //NSLog("back camera onCapture")
+//            return frame
+//        }
+//        backCameraHandlers.onStart = {
+//            NSLog("back camera onStart")
+//        }
+//        backCameraHandlers.onStop = {
+//            NSLog("back camera onStop")
+//        }
+//
+//        CameraVideoCapturer.front.handlers = frontCameraHandlers
+//        CameraVideoCapturer.back.handlers = backCameraHandlers
+
+        CameraVideoCapturer.handlers.onStart = { capturer in
+            // カメラ位置ごとの処理
+            switch capturer.position {
+            case .front:
+                // 前面カメラ時
+                NSLog("# front camera start")
+            case .back:
+                // 背面カメラ時
+                NSLog("# back camera start")
+            default:
+                break
+            }
+            // カメラ位置に依存しない処理
+            NSLog("# capturer start => \(capturer)")
+        }
+
+        CameraVideoCapturer.handlers.onStop = { capturer in
+            // カメラ位置ごとの処理
+            switch capturer.position {
+            case .front:
+                // 前面カメラ時
+                NSLog("# front camera stop")
+            case .back:
+                // 背面カメラ時
+                NSLog("# back camera stop")
+            default:
+                break
+            }
+            // カメラ位置に依存しない処理
+            NSLog("# capturer stop => \(capturer)")
+        }
+        CameraVideoCapturer.handlers.onCapture = { capturer in
+            // カメラ位置ごとの処理
+            switch capturer.position {
+            case .front:
+                // 前面カメラ時
+                NSLog("# front camera stop")
+            case .back:
+                // 背面カメラ時
+                NSLog("# back camera stop")
+            default:
+                break
+            }
+            // カメラ位置に依存しない処理
+            NSLog("# capturer stop => \(capturer)")
         }
 
         // 接続します。
@@ -106,12 +195,79 @@ class ViewController: UIViewController {
             // 接続に成功した MediaChannel を保持しておきます。
             self.mediaChannel = mediaChannel
 
+            
             // 接続できたら配信用の VideoView をストリームにセットします。
-            if let stream = mediaChannel!.senderStream {
-                stream.videoRenderer = self.senderVideoView
-            }
+//            if let stream = mediaChannel!.senderStream {
+//                stream.videoRenderer = self.senderVideoView
+//            }
         }
     }
 
+    
+    @IBAction func start(_ sender: Any) {
+        guard let stream = mediaChannel!.senderStream else {
+            return
+        }
+        stream.videoRenderer = self.senderVideoView
+        let vga = CameraSettings.Resolution.vga480p
+        self.camera = isFront ? CameraVideoCapturer.front : CameraVideoCapturer.back
+        let format = CameraVideoCapturer.format(width: vga.width, height: vga.height, for: self.camera!.device!)
+        self.camera!.start(format: format!, frameRate: 30) { error in
+            if let error = error {
+                NSLog(error.localizedDescription)
+                return
+            }
+            CameraVideoCapturer.current!.stream = stream
+        }
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        CameraVideoCapturer.current?.stop() { error in
+            if let error = error {
+                NSLog(error.localizedDescription)
+            }
+        }
+    }
+    
+    @IBAction func flip(_ sender: Any) {
+        CameraVideoCapturer.flip(CameraVideoCapturer.current!) { error in
+            if let error = error {
+                NSLog(error.localizedDescription)
+                return
+            }
+            self.isFront = self.isFront ? false : true
+        }
+        NSLog("flip camera")
+    }
+    
+    @IBAction func restart(_ sender: Any) {
+        if self.isFront == true{
+            CameraVideoCapturer.front.restart() { error in
+                if let error = error {
+                    NSLog(error.localizedDescription)
+                }
+            }
+            NSLog("restart front camera")
+        } else {
+            CameraVideoCapturer.back.restart() { error in
+                if let error = error {
+                    NSLog(error.localizedDescription)
+                }
+            }
+            NSLog("restart back camera")
+        }
+    }
+    
+    @IBAction func change(_ sender: Any) {
+        let hd = CameraSettings.Resolution.hd1080p
+        let format = CameraVideoCapturer.format(width: hd.width, height: hd.height, for: CameraVideoCapturer.current!.device!)
+        CameraVideoCapturer.current?.change(format: format, frameRate: 1) { error in
+            if let error = error {
+                NSLog(error.localizedDescription)
+                return
+            }
+        }
+        NSLog("change camera")
+    }
 }
 
